8e3a9d88c23bb2d52423397746510fbdb6aa7549
YashJM <mistryyash100@gmail.com>
2023-03-07 01:08:31 -0500

    Code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import { observer } from 'mobx-react';
 import userStore from './store/dataStore/userStore/userStore';
 
@@ -10,12 +9,6 @@ function App() {
     <div className="App">
       <div>
         <p>Username from store: {`${userStore.user.firstName} ${userStore.user.lastName}`}</p>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -26,9 +19,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
